940c3e5b7c36ddacb514e86a1caea1c0f4799d25
dlworua <leegaegyum@gmail.com>
2025-04-01 21:28:16 +0900

    feat:haha

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyHomePage extends StatefulWidget {
   //암 미ㅣ암
 
   // This class is the configuration for the state. It holds the values (in this
-  // case thee App widget) and
+  // case th
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
