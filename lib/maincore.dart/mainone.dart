8909c402aeb9eb993b296ebc0432b59314ab349a
dlworua <leegaegyum@gmail.com>
2025-04-02 19:35:48 +0900

    feat:사과

--- lib/maincore.dart/mainone.dart
+++ lib/maincore.dart/mainone.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 class maincore extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      title: Text('어서오세요'),
-      titleTextStyle: TextStyle(color: Colors.red),
+    return Column(
+      children: [
+        AppBar(
+          title: Text('어서오세요'),
+          titleTextStyle: TextStyle(color: Colors.red, fontSize: 39),
+        ),
+      ],
     );
   }
 }
-
-//hah
-//할수있어!
