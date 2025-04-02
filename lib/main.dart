b63550959f9115fcf4d95cdd889a65bd21330add
dlworua <leegaegyum@gmail.com>
2025-04-02 19:19:52 +0900

    feat:그냥

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_pro/maincore.dart/mainone.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(home: maincore());
   }
 }
--- lib/maincore.dart/mainone.dart
+++ lib/maincore.dart/mainone.dart
@@ -9,3 +9,5 @@ class maincore extends StatelessWidget {
     );
   }
 }
+
+//hah
